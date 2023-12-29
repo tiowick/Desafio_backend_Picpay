b75484c0c32b55d7341ba561220b897b66ae6b0d
Jeferson sena <jefinho368@gmail.com>
2023-12-29 08:15:38 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/picpaydesafio/repositories/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.picpaydesafio.repositories;
+
+import com.picpaydesafio.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
