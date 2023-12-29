44ff74a0ec0a4ce8266ed4ef58002b88243c4b99
Jeferson sena <jefinho368@gmail.com>
2023-12-29 08:09:36 -0300

    feat: initial commit

---
+++ src/main/java/com/picpaydesafio/PicpayDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.picpaydesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpaydesafio/PicpayDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaydesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
