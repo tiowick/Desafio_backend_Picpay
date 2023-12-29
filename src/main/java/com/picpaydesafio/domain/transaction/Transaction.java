f0b1ae69d981caa15e40ddfe20cfbf6e71185ee6
Jeferson sena <jefinho368@gmail.com>
2023-12-29 12:42:21 -0300

    feat: create Transaction and TransactionController.java

--- src/main/java/com/picpaydesafio/domain/transaction/Transaction.java
+++ src/main/java/com/picpaydesafio/domain/transaction/Transaction.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "transactions")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Transaction {
 
